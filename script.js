d7c6f1e827f4202b810134a8ac589db9abbbd31d
CameronTH <cameronthomson40@gmail.com>
2023-11-13 16:21:19 +0000

    Started working on function to find the winning combinations on the board and started working on menu dialog.

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const boxes = document.querySelectorAll(".box");
+const container = document.querySelector(".container");
+const dialog = document.querySelector(".dialog");
 
 const gameBoard = (function () {
   let currentBoard = ["", "", "", "", "", "", "", "", ""];
 
+  let isGameOver = false;
+
+  let isGameReset = false;
+
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
   const resetBoard = () => {
     gameBoard.currentBoard = ["", "", "", "", "", "", "", "", ""];
+    boxes.forEach((box) => {
+      box.textContent = "";
+      box.classList.remove("x");
+      box.classList.remove("o");
+    });
   };
 
   const createPlayerProfile = function (
@@ -21,64 +43,86 @@ const gameBoard = (function () {
     return { player, type, isComputer, isTurn };
   };
 
-  return { currentBoard, resetBoard, createPlayerProfile };
+  return {
+    currentBoard,
+    isGameOver,
+    winningCombos,
+    resetBoard,
+    createPlayerProfile,
+  };
 })();
 
 const randomNum = function () {
   let rand = Math.floor(Math.random() * 9);
-  while (
-    boxes[rand].textContent !== "" &&
-    gameBoard.currentBoard.includes("")
-  ) {
+  while (boxes[rand].textContent !== "" && gameBoard.currentBoard.includes(""))
     rand = Math.floor(Math.random() * 9);
-  }
 
   return rand;
 };
 
 const gameController = (function () {
-  boxes.forEach((box) => {
+  boxes.forEach((box, index) => {
     function handleClick() {
-      console.log("pressed");
       if (player1.isTurn === true) {
         gameBoard.currentBoard[box.getAttribute("data-number")] = player1.type;
         box.textContent = player1.type;
+        box.classList.add("x");
         player1.isTurn = false;
-        boxStopClick();
-        if (player2.isComputer === true) {
+        checkWinStatus();
+        // boxStopClick();
+
+        if (player2.isComputer === true && gameBoard.isGameOver === false) {
           let randomNumber = randomNum();
-          console.log("this" + randomNumber);
           gameBoard.currentBoard[
             boxes[randomNumber].getAttribute("data-number")
           ] = player2.type;
           boxes[randomNumber].textContent = player2.type;
-          boxes[randomNumber].removeEventListener("click", handleClick);
+          boxes[randomNumber].classList.add("o");
+          boxStop2(randomNumber);
           player1.isTurn = true;
-          console.log(gameBoard.currentBoard);
         }
       }
     }
-    box.addEventListener("click", handleClick);
 
-    const boxStopClick = () => {
-      box.removeEventListener("click", handleClick);
+    box.addEventListener("click", handleClick);
+    boxes[1].removeEventListener("click", handleClick);
+    const boxStop2 = (num) => {
+      console.log("work");
+
+      boxes[0].removeEventListener("click", handleClick);
     };
+
+    // const boxStopClick = () => box.removeEventListener("click", handleClick);
   });
 })();
 
-// const playerTurn = function (box) {
-//   gameBoard.currentBoard[box.getAttribute("data-number")] = player1.type;
-//   box.textContent = player1.type;
-//   player1.isTurn = false;
-// };
+// gameController();
 
-// const computerTurn = function () {
-//   const randomNum = Math.floor(Math.random() * 9);
-//   gameBoard.currentBoard[boxes[randomNum].getAttribute("data-number")] =
-//     player2.type;
-//   boxes[randomNum].textContent = player2.type;
-//   player1.isTurn = true;
-// };
+const checkWinStatus = function () {
+  gameBoard.winningCombos.forEach((array) => {
+    const crossWins = array.every((cell) => boxes[cell].textContent === "x");
+
+    if (crossWins) {
+      gameBoard.isGameOver = true;
+      gameMenu(player1);
+    }
+  });
+};
+
+const gameMenu = function (winningPlayer) {
+  dialog.showModal();
+  const startButton = document.querySelector(".dialog-start");
+  const status = document.querySelector(".status");
+  startButton.textContent = "Play Again";
+  status.textContent = `The Winner is ${winningPlayer.player}!`;
+  startButton.addEventListener("click", () => {
+    gameBoard.resetBoard();
+    player1.isTurn = true;
+    gameBoard.isGameOver = false;
+    // gameController();
+    dialog.close();
+  });
+};
 
 const player1 = gameBoard.createPlayerProfile("cammy", "x", false, true);
 const player2 = gameBoard.createPlayerProfile("computer", "o", true, false);
